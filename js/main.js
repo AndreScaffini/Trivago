b6996d4af61236aea6466363ec8ff530b3d2e01a
AndreScaffini <andrescaffini@gmail.com>
2024-03-03 20:46:47 -0600

    Responsive dising upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+document.getElementById('menu-toggle').addEventListener('click', function() {
+    var menu = document.getElementById('menu');
+    menu.classList.toggle('hidden');
+});
+document.getElementById('menu-toggle2').addEventListener('click', function() {
+    var menu = document.getElementById('menu2');
+    menu.classList.toggle('hidden');
+});
